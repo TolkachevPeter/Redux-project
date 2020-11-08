60a547f1d6e877917a58ad63dea4533cb6ce3145
TolkachevPeter <peter.tolkachev@gmail.com>
2020-11-08 18:16:51 +0300

    fix booksRequested

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,7 +3,7 @@ import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 
 import { withBookstoreService } from '../hoc';
-import { booksLoaded } from '../../actions';
+import { booksLoaded, booksRequested } from '../../actions';
 import { compose } from '../../utils';
 import Spinner from '../spinner';
 import './book-list.css';
@@ -12,7 +12,8 @@ class BookList extends Component {
 
   componentDidMount() {
 
-    const { bookstoreService, booksLoaded } = this.props;
+    const { bookstoreService, booksLoaded, booksRequested } = this.props;
+    booksRequested();
     bookstoreService.getBooks()
       .then((data) => booksLoaded(data));
   }
@@ -43,7 +44,8 @@ const mapStateToProps = ({ books, loading }) => {
 };
 
 const mapDispatchToProps = {
-  booksLoaded
+  booksLoaded,
+  booksRequested
 };
 
 export default compose(
