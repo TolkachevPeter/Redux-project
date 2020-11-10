3bca4a06616c93d9efeb136f1dc9c3db4bdb30a8
TolkachevPeter <peter.tolkachev@gmail.com>
2020-11-10 20:38:47 +0300

    fix actions

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -3,7 +3,7 @@ import BookListItem from '../book-list-item';
 import { connect } from 'react-redux';
 
 import { withBookstoreService } from '../hoc';
-import { booksLoaded, booksRequested, booksError } from '../../actions';
+import { fetchBooks } from '../../actions';
 import ErrorIndicator from '../error-indicator';
 import { compose } from '../../utils';
 import Spinner from '../spinner';
@@ -44,15 +44,9 @@ const mapStateToProps = ({ books, loading, error }) => {
   return { books, loading, error };
 };
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-  const { bookstoreService } = ownProps;
+const mapDispatchToProps = (dispatch, { bookstoreService }) => {
   return {
-    fetchBooks: () => {
-    dispatch(booksRequested());
-    bookstoreService.getBooks()
-      .then((data) => dispatch(booksLoaded(data)))
-      .catch((err) => dispatch(booksError(err)));
-    }
+    fetchBooks: fetchBooks(bookstoreService, dispatch)
   }
 };
 
