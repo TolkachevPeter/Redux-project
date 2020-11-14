b82250314749c2a45496aab7eb3e721989ae41a1
TolkachevPeter <peter.tolkachev@gmail.com>
2020-11-14 21:41:30 +0300

    add new actions

--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './shopping-cart-table.css';
+import { bookAddedToCart, bookRemovedFromCart, AllBooksRemovedFromCart } from '../../actions';
 
 const ShoppingCartTable = ({
   items, 
