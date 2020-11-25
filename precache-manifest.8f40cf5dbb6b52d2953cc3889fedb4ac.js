bf2c69ecb68d2bcfbb5c391b4ce7a0e2ac008769
TolkachevPeter <peter.tolkachev@gmail.com>
2020-11-25 21:23:04 +0300

    Updates

---
+++ precache-manifest.8f40cf5dbb6b52d2953cc3889fedb4ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a076bd4960d1f4f5ac41672cd544dc4",
+    "url": "/TolkachevPeter/Redux-project/index.html"
+  },
+  {
+    "revision": "cb880b1dd97926a5ae70",
+    "url": "/TolkachevPeter/Redux-project/static/css/main.9a410b61.chunk.css"
+  },
+  {
+    "revision": "41166ff4d0ff36b0a6cb",
+    "url": "/TolkachevPeter/Redux-project/static/js/2.aa5f393b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TolkachevPeter/Redux-project/static/js/2.aa5f393b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb880b1dd97926a5ae70",
+    "url": "/TolkachevPeter/Redux-project/static/js/main.057eca66.chunk.js"
+  },
+  {
+    "revision": "12dee0c187055174c1f5",
+    "url": "/TolkachevPeter/Redux-project/static/js/runtime-main.0697c0b0.js"
+  }
+]);
